9cc054d8667cae20b50e9927be46c666a3c3581d
Benjamin Williamson <benjamin.williamson.10@ucl.ac.uk>
2024-08-22 13:24:27 +0200

    eigenvectors to eigendisplacements was wrong! now changed to i*3:i*3+3

--- puppy/unfolder.py
+++ puppy/unfolder.py
@@ -6,6 +6,7 @@ import itertools as it
 import copy 
 from phonopy import load
 import numpy as np 
+import pandas as pd 
 from phonopy.unfolding.core import Unfolding
 from phonopy.phonon.band_structure import get_band_qpoints_by_seekpath
 from pymatgen.io.phonopy import eigvec_to_eigdispl
@@ -38,20 +39,94 @@ class PhononUnfoldingandProjection:
                 with gzip.open(file, 'rb') as f_in, open(file.replace('.gz', ''), 'wb') as f_out:
                     f_out.writelines(f_in)
 
-    def save_poscar_with_only_neighbours(self,filename='poscar_reduced.vasp',use_dummy_atom='K'):
+
+    def save_poscar_with_only_neighbours(self,filename='poscar_reduced.vasp',
+                                         use_dummy_atom='K',
+                                         with_vectors=False,
+                                         vector_args=None,
+                                         scale=2):
         struct = Structure.from_file(self.defect_directory+'SPOSCAR')
-        nn = self.get_neighbour_sites()
+        eigenvectors = self.defect_band_data['eigenvectors']
+        host_frequencies = self.host_band_data['frequencies']
+        
+        def _grab_chosen_vectors(nearest_neighbours):
+        
+            from pymatgen.io.phonopy import eigvec_to_eigdispl    
+
+            #vector_args = {'qpt':0,'line':0,'band':5,'threshold':0.1,'tolerance':0.05}        
+            if not vector_args['tolerance']:
+                tolerance = 0.1
+            else:
+                tolerance = vector_args['tolerance']    
+
+            if not vector_args['threshold']:
+                threshold = 0.1
+            else:
+                threshold = vector_args['threshold']    
+
+            chosen_frequency = host_frequencies[vector_args['qpt']][vector_args['line']][vector_args['freq']]            
+
+            unfolded_frequencies = self.unfold_data['f'] 
+            unfolded_weights = self.unfold_data['w']
+            eigenvectors = self.defect_band_data['eigenvectors']            
+
+            chosen_indexes = {}
+            for i,(f,w) in enumerate(zip(unfolded_frequencies[vector_args['qpt']][vector_args['line']],
+                           unfolded_weights[vector_args['qpt']][vector_args['line']])):
+                if w>=threshold:
+                    if np.isclose(f,chosen_frequency,tolerance):
+                        chosen_indexes[i] = {'f':f,'w':w}        
+
+            df = pd.DataFrame(chosen_indexes).T.sort_values(by='w',ascending=False)     
+            print(df)       
+
+            chosen_index = df.index[0]            
+
+            atom_coords = self.defect_phonons.supercell.get_scaled_positions()
+            masses = self.defect_phonons.supercell.get_masses()
+            qpts = self.defect_band_data['qpoints']       
+
+            atom_indexes = []
+            for i in nn.values():
+                for j in i:
+                    atom_indexes.append(j)            
+
+            eigendisplacements = []
+            for i in range(len(masses)):
+                eigendisplacements.append(
+                    eigvec_to_eigdispl(eig_vec=eigenvectors[vector_args['qpt']][vector_args['line']][chosen_index][i*3:i*3+3],
+                                       q=qpts[vector_args['qpt']][vector_args['line']],
+                                       frac_coords=atom_coords[i],
+                                       mass=masses[i]
+                                       )
+                )            
+
+            chosen_vectors = []
+            max_disp = np.max([np.linalg.norm(x) for x in eigendisplacements])
+            for atom in range(len(atom_coords)):
+                chosen_vectors.append([(np.real(x)/max_disp)*scale for x in eigendisplacements[atom]]) # may change max value     to the         max of the atom...            
+#/np.real(np.max(eigendisplacements[atom])))*scale
+            return(chosen_vectors)
+        
+        nn = self.get_neighbour_sites()    
+
         indexes = []
         for x in nn.values():
             for ix in x:
                 indexes.append(ix)
         non_indexes = [x for x in range(len(struct)) if x not in list(indexes)]
+        if with_vectors and vector_args:
+            chosen_vectors = _grab_chosen_vectors(nn)
+            print(chosen_vectors)
+            struct.add_site_property('magmom',chosen_vectors)    
+
         struct.remove_sites(non_indexes)
         if use_dummy_atom:
-            struct.append(species=use_dummy_atom,coords=self.defect_site.frac_coords)
-        struct.to(filename=filename,fmt='poscar')
+            struct.append(species=use_dummy_atom,
+                          coords=self.defect_site.frac_coords)
+        struct.to(filename=filename, fmt='poscar')
         print('vacancy "cage" saved to {}.'.format(filename))
-        return(struct)
+        return (struct)
 
     def get_neighbour_sites(self):
             
@@ -133,7 +208,7 @@ class PhononUnfoldingandProjection:
                             eigdispl = [
                                 np.linalg.norm(
                                     eigvec_to_eigdispl(
-                                        freq[site:site+3],
+                                        freq[site*3:site*3+3], # [site:site+3]
                                         q=qpts[i][ii],
                                         frac_coords=atom_coords[site],
                                         mass=masses[site])
